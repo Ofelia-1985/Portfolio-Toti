ea45179f1f29f3c4451aa72f3533c8c658ef3ff3
Carmen Urbina Lopez <81659893+Ofelia-1985@users.noreply.github.com>
2025-12-03 18:47:12 -0300

    Add modo dark e toggle section

--- portfolio.js
+++ portfolio.js
@@ -7,3 +7,41 @@ btn.addEventListener("click", () => {
     ? "Mostrar boas-vindas"
     : "Ocultar boas-vindas";
 });
+
+/* ========= SECTIONS TOOGLE ============= */
+
+const sections = document.querySelectorAll("main section");
+
+sections.forEach((section) => {
+  const title = section.querySelector("h2");
+  const content = section.querySelector("div");
+
+      content.classList.add("collapse-content");
+      title.style.cursor = "pointer";
+
+  title.addEventListener("click", () => {
+    content.classList.toggle("open");
+    title.classList.toggle("active");
+  });
+});
+
+/* =============== MODO DARK =============== */
+
+const toggle = document.getElementById("theme-toggle");
+const body = document.body;
+
+if (localStorage.getItem("theme") === "light") {
+  body.classList.add("light-mode");
+}
+
+toggle.addEventListener("click", () => {
+  body.classList.toggle("light-mode");
+
+  if (body.classList.contains("light-mode")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.removeItem("theme");
+  }
+});
+
+
